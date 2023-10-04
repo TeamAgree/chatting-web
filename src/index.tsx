1937dc2f7ef06859aaf9780b84e4b9834813ec68
ANCHANIK <cksdlr7446@gmail.com>
2023-10-04 08:58:37 +0000

    feat: axios default 설정

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { RecoilRoot } from 'recoil';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
+
+axios.defaults.baseURL =
+  process.env.NODE_ENV === 'production'
+    ? ''
+    : 'http://agree.iptime.org/api/v1';
+axios.defaults.headers['Access-Control-Allow-Origin'] =
+  'http://agree.iptime.org:3000';
+axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
