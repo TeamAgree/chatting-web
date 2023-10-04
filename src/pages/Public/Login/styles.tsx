441a3200c5739583f1e06bf5e644de6281753981
ANCHANIK <cksdlr7446@gmail.com>
2023-10-04 08:58:52 +0000

    style: 로그인, 회원가입 스타일 적용

--- src/pages/Public/Login/styles.tsx
+++ src/pages/Public/Login/styles.tsx
@@ -6,14 +6,41 @@ export const LoginWrap = styled.section`
   display: flex;
   justify-content: center;
   align-items: center;
-  background: #eee;
+  background: #c1d7c6;
 
   div.container {
-    padding: 5rem;
+    padding: 3rem;
 
     min-width: 20rem;
+    height: 18rem;
+    max-height: 18rem;
     background: #fff;
     box-shadow: 0px 0px 5px 0px #54545466;
+
+    input {
+      margin-bottom: 3px;
+
+      height: 2.5rem;
+
+      border: none;
+      border-bottom: 1px solid #eaeaea;
+
+      text-indent: 10px;
+    }
+
+    button {
+      font-size: 1.3rem;
+      color: #349753;
+    }
+
+    div.link_wrap {
+      text-align: center;
+      a {
+        padding: 1rem;
+        color: #a1a1a1;
+        font-size: 13px;
+      }
+    }
   }
 `;
 
@@ -23,17 +50,48 @@ export const SignUpWrap = styled.section`
   display: flex;
   justify-content: center;
   align-items: center;
-  background: #eee;
+  background: #c1d7c6;
 
   div.container {
-    padding: 5rem;
+    padding: 3rem;
 
     min-width: 20rem;
+    height: 35rem;
+    max-height: 100%;
     background: #fff;
     box-shadow: 0px 0px 5px 0px #54545466;
+
+    input {
+      margin-bottom: 3px;
+
+      height: 2.5rem;
+
+      border: none;
+      border-bottom: 1px solid #eaeaea;
+
+      text-indent: 10px;
+    }
+
+    button {
+      font-size: 1.3rem;
+      color: #349753;
+    }
+
+    div.link_wrap {
+      text-align: center;
+      a {
+        padding: 1rem;
+        color: #a1a1a1;
+        font-size: 13px;
+      }
+    }
   }
 `;
 
 export const Form = styled.form`
-  display: grid;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: space-between;
+
+  height: 100%;
 `;
